cda794dcc26485b280707a9920260765a6cf6684
Labib Fawaz <lfawaz@inflocasersmbp4.attlocal.net>
2016-11-27 22:46:42 -0500

    add dataset

--- Final Project/fetchTweets/src/main/scala/twitterAPITest.scala
+++ Final Project/fetchTweets/src/main/scala/twitterAPITest.scala
@@ -16,27 +16,24 @@ object twitterAPITest {
           .setOAuthAccessToken("2657538169-EZyrslwb7Pqztaa9NO8V92UfVZpUYsW11TLbj4V")
           .setOAuthAccessTokenSecret("urEAIGHu8ZEnSYUjhejUz6LnL6fCd6MO8VIkObWgDYNN1")
 
-        val YahooFileName = "YahootweetFile" + Calendar.getInstance.getTime.toString() + ".txt"
-        val Yahoopw = new PrintWriter(new File(YahooFileName))
 
-        val SamsungFileName = "SamsungtweetFile" + Calendar.getInstance.getTime.toString() + ".txt"
-        val Samsungpw = new PrintWriter(new File(SamsungFileName))
+        val query = "Samsung"
 
         val tw = new TwitterFactory(cb.build()).getInstance
-        val Yahoostatuses = tw.search(new Query("Yahoo")).getTweets.filter(_.getLang == "en")
-        val Samsungstatuses = tw.search(new Query("Samsung")).getTweets.filter(_.getLang == "en")
+
+        val FileName = query + "tweetFile" + Calendar.getInstance.getTime.toString() + ".txt"
+        val pw = new PrintWriter(new File(FileName))
+
+
+        val statuses = tw.search(new Query(query)).getTweets.filter(_.getLang == "en")
         //statuses.foreach(s => println(s.getText))
 
         //take 20 tweets and produce sentiment score
-        val Yahootweets = Yahoostatuses.take(20).map(Utils.extractText(_)).map(t => (t, SentimentAnalyzer.mainSentiment(t)))
-        Yahootweets.foreach(println)
-        Yahoopw.write(Yahootweets.toString())
-        Yahoopw.close()
+        val tweets = statuses.take(20).map(Utils.extractText(_)).map(t => (t, SentimentAnalyzer.mainSentiment(t)))
+          tweets.foreach(println)
+          pw.write(tweets.toString())
+          pw.close()
 
-        val Samsungtweets = Samsungstatuses.take(20).map(Utils.extractText(_)).map(t => (t, SentimentAnalyzer.mainSentiment(t)))
-        Samsungtweets.foreach(println)
-        Samsungpw.write(Samsungtweets.toString())
-        Samsungpw.close()
 
 
 
